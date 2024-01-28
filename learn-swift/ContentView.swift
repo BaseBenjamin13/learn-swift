1c06b796f1ce57631d8ec67ccab0fe30af2c41e1
BaseBenjamin13 <benmorgiewicz@gmail.com>
2024-01-28 11:22:58 -0500

    Initial Commit

---
+++ learn-swift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  learn-swift
+//
+//  Created by Ben Morgiewicz on 1/28/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ learn-swift/learn_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  learn_swiftApp.swift
+//  learn-swift
+//
+//  Created by Ben Morgiewicz on 1/28/24.
+//
+
+import SwiftUI
+
+@main
+struct learn_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
